4287eac3fee5225ba76610ad9d46b7ed4cb181ad
sozdatel31 <pavlik.gerasim@gmail.com>
2021-06-27 21:44:06 +0300

    registration3 (no design)

--- src/Components/Registration/Registration.tsx
+++ src/Components/Registration/Registration.tsx
@@ -2,9 +2,10 @@ import React, {useEffect, useState} from 'react';
 import SuperInputText from "../../SuperComponents/c1-SuperInputText/SuperInputText";
 import SuperButton from "../../SuperComponents/c2-SuperButton/SuperButton";
 import {useDispatch, useSelector} from "react-redux";
-import {registerTC, setIsInitializedAC} from "../../Redux/reg-reducer";
+import {registerTC, setAppErrorAC, setIsInitializedAC} from "../../Redux/reg-reducer";
 import {Redirect} from 'react-router-dom';
 import {AppStateType} from "../../Redux/store";
+import style from "./Registration.module.css"
 
 export function Registration() {
 //useState
@@ -14,15 +15,21 @@ export function Registration() {
 //useSelectors
     const isInitialized = useSelector<AppStateType, boolean>((state) => state.reg.isInitialized)
     const status = useSelector<AppStateType, string>((state) => state.reg.status)
+    const error = useSelector<AppStateType, string | null>((state) => state.reg.error)
 
     const dispatch = useDispatch()
 
     const buttonCallback = () => {
-        dispatch(registerTC(email, password))
+        if (password === passwordConf) {
+            dispatch(registerTC(email, password))
+        } else {
+            dispatch(setAppErrorAC("Password mismatch"))
+        }
     }
 
     useEffect(() => {
         dispatch(setIsInitializedAC(false))
+        dispatch(setAppErrorAC(null))
     }, [isInitialized])
 
     if (isInitialized) {
@@ -31,7 +38,11 @@ export function Registration() {
     return (
 
         <div>
+
             <h1>Registration</h1>
+            {error && <div className={style.formSummaryError}>
+                {error}
+            </div>}
             <div>Email:</div>
             <SuperInputText value={email} onChangeText={setEmail}/>
             <div>password:</div>
@@ -39,7 +50,7 @@ export function Registration() {
             <div>confirm password:</div>
             <SuperInputText value={passwordConf} onChangeText={setPasswordConf}/>
             <div>
-                <SuperButton disabled={status!=='idle'} onClick={buttonCallback}>Sign Up</SuperButton>
+                <SuperButton disabled={status !== 'idle'} onClick={buttonCallback}>Sign Up</SuperButton>
             </div>
 
         </div>
