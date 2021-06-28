45519197dec14847135d05f4505acc869460de32
pavel.kv94 <pavel.kv94@yandex.by>
2021-06-28 13:55:57 +0300

    feat: show data of logged user in Profile

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { ResponseLoginType } from '../../api/fridayProject-api';
+import { AppStateType } from '../../Redux/store';
 
 export function Profile() {
+    
+    const data = useSelector<AppStateType, ResponseLoginType>(state => state.loginPage.userData)
+
     return (
 
         <div>
             <h1>Profile</h1>
-
+            <br />
+            <h3>Данные пользователя</h3>
+            <p>email - {data.email}</p>
+            <p>name - {data.name}</p>
+            <p>created - {data.created}</p>
+            <p>updated - {data.updated}</p>
+            <p>count of cards - {data.publicCardPacksCount}</p>
         </div>
     );
 }
