6046982da2420281dd886f4e45e29d546026994d
Pavelkv94 <pavel.kv94@yandex.by>
2021-07-19 13:23:14 +0300

    style: add css for passwordRecovery

--- src/Components/PasswordRecovery/PasswordRecovery.tsx
+++ src/Components/PasswordRecovery/PasswordRecovery.tsx
@@ -1,18 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import SuperInputText from "../../SuperComponents/c1-SuperInputText/SuperInputText";
 import SuperButton from "../../SuperComponents/c2-SuperButton/SuperButton";
-import {useDispatch, useSelector} from "react-redux";
-import {sendTokenTC} from "../../Redux/password-recovery-reducer";
+import { useDispatch, useSelector } from "react-redux";
+import { sendTokenTC } from "../../Redux/password-recovery-reducer";
 import style from "../Registration/Registration.module.css";
-import {AppStateType} from "../../Redux/store";
-import {Redirect} from "react-router-dom";
+import { AppStateType } from "../../Redux/store";
+import { NavLink, Redirect } from "react-router-dom";
 import Preloader from "../../Common/Preloader/Preloader";
+import s from './PasswordRecovery.module.css'
+import { PATH } from '../../Routes';
 
 export function PasswordRecovery() {
     //useState
     const [email, setEmail] = useState<string>("")
 
-//useSelectors
+    //useSelectors
     const isInitialized = useSelector<AppStateType, boolean>((state) => state.recovPass.isInitialized)
     const status = useSelector<AppStateType, string>((state) => state.app.status)
     const isAuth = useSelector<AppStateType, string>(state => state.loginPage.isAuth)
@@ -28,20 +30,29 @@ export function PasswordRecovery() {
         return <div>OOOkay, check email</div>
     }
     if (isAuth) {
-        return <Redirect to='/profile'/>
+        return <Redirect to='/profile' />
     }
 
     return (
-        <div>
-            {status !== 'idle' ? <Preloader/> : null}
-            <h1>PasswordRecovery</h1>
-            {error && <div className={style.formSummaryError}>
-                {error}
-            </div>}
-            <div>Email:</div>
-            <SuperInputText value={email} onChangeText={setEmail} isType="password"/>
-            <div>
-                <SuperButton disabled={status !== "idle"} onClick={buttonCallback}>SEND</SuperButton>
+        <div className={s.container}>
+            <div className={s.recoveryContainer}>
+                <h2 className={s.title}>It-incubator</h2>
+                <p className={s.subTitle}>Forgot your password?</p>
+                {status !== 'idle' ? <Preloader /> : null}
+                {error && <div className={style.formSummaryError}>
+                    {error}
+                </div>}
+                <SuperInputText value={email} onChangeText={setEmail} isType="email" />
+                <div className={s.info}>
+                    <p>Enter your email address and we will send you further instructions </p>
+                </div>
+                <div>
+                    <SuperButton style={{ width: "266px", marginTop: "100px" }} disabled={status !== "idle"} onClick={buttonCallback}>SEND</SuperButton>
+                </div>
+                <div className={s.info}>
+                    <p>Did you remember your password?</p>
+                </div>
+                <div className={s.goLogin}><NavLink to={PATH.LOGIN}>Try logging in</NavLink></div>
             </div>
         </div>
     );
